d61b1b2da25bdb5ce9cb4f21c16f5346445b1ff3
marcuscastelo <viniciuscastelo6@gmail.com>
2021-10-09 08:47:40 -0300

    Adding user input check utility

---
+++ user_input.py
@@ -0,0 +1,37 @@
+import keyboard
+import mouse
+import pyautogui
+import time
+
+keyboardPressed = False
+
+def on_press(key):
+    global keyboardPressed
+    print('[USER INPUT] Keyboard is in use: ', key)
+    keyboardPressed = True
+keyboard.on_press(on_press)
+
+def is_user_currently_using_keyboar_or_mouse():
+    """
+    Returns True if the user is currently using the keyboard or mouse.
+    """
+    global keyboardPressed
+    keyboardPressedAtStart = keyboardPressed
+    keyboardPressed = False
+
+    mousePosBefore = pyautogui.position()
+    time.sleep(0.05)
+    mousePosAfter = pyautogui.position()
+
+    mouseMoved = (
+        mousePosBefore != mousePosAfter
+        or
+        mouse.is_pressed('left')
+        or
+        mouse.is_pressed('right')
+    )
+
+    keyboardPressedAtEnd = keyboardPressed
+    keyboardPressed = False
+    
+    return keyboardPressedAtStart or keyboardPressedAtEnd or mouseMoved
