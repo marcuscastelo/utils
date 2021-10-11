818082e18d13436fb3d26a7ee545fbf81e1d4ea3
marcuscastelo <viniciuscastelo6@gmail.com>
2021-10-11 08:37:56 -0300

    Changing Rect Class

--- geometry.py
+++ geometry.py
@@ -4,6 +4,10 @@ from typing import Union
 import bot_base.util.colors as colors
 from bot_base.util.serialization import Serializable
 
+from copy import copy
+
+import numpy as np
+
 @dataclass
 class Vec2:
     x: int
@@ -84,6 +88,177 @@ class Rect(Serializable):
     y: int
     width: int
     height: int
+
+    def __post_init__(self):
+        self.x = int(self.x)
+        self.y = int(self.y)
+        self.width = int(self.width)
+        self.height = int(self.height)
+
+    def __repr__(self):
+        return f'Rect({self.x}, {self.y}, {self.width}, {self.height})'
+
+    def __iter__(self):
+        yield from (self.x, self.y, self.width, self.height)
+
+    def __getitem__(self, item):
+        assert isinstance(item, int)
+        return (self.x, self.y, self.width, self.height)[item]
+    
+    def __setitem__(self, key, value):
+        if key == 0:
+            self.x = value
+        elif key == 1:
+            self.y = value
+        elif key == 2:
+            self.width = value
+        elif key == 3:
+            self.height = value
+        else:
+            raise IndexError
+
+    def __add__(self, vec: 'Vec2') -> 'Rect':
+        assert isinstance(vec, Vec2)
+        newRect = copy(self)
+        newRect.x += vec.x
+        newRect.y += vec.y
+        return newRect
+
+    def __sub__(self, vec: 'Vec2') -> 'Rect':
+        assert isinstance(vec, Vec2)
+        newRect = copy(self)
+        newRect.x -= vec.x
+        newRect.y -= vec.y
+        return newRect
+
+    def center(self) -> 'Vec2':
+        return Vec2(self.x + self.width // 2, self.y + self.height // 2)
+
+    def pivot(self) -> 'Vec2':
+        return Vec2(self.x, self.y)
+
+    def size(self) -> 'Vec2':
+        return Vec2(self.width, self.height)
+    
+    def to_bbox(self) -> tuple:
+        return (self.x, self.y, self.width + self.x, self.height + self.y)
+
+    def top_left(self) -> Vec2:
+        return Vec2(self.x, self.y)
+
+    def top_right(self) -> Vec2:
+        return Vec2(self.x + self.width, self.y)
+
+    def bottom_left(self) -> Vec2:
+        return Vec2(self.x, self.y + self.height)
+
+    def bottom_right(self) -> Vec2:
+        return Vec2(self.x + self.width, self.y + self.height)
+
+    def with_x(self, x: int) -> 'Rect':
+        newRect = copy(self)
+        newRect.x = x
+        return copy 
+
+    def with_y(self, y: int) -> 'Rect':
+        newRect = copy(self)
+        newRect.y = y
+        return copy
+
+    def with_pivot(self, pivot: 'Vec2') -> 'Rect':
+        newRect = copy(self)
+        newRect.x = pivot.x - self.width // 2
+        newRect.y = pivot.y - self.height // 2
+        return newRect
+
+    def with_width(self, width: int) -> 'Rect':
+        newRect = copy(self)
+        newRect.width = width
+        return newRect
+
+    def with_height(self, height: int) -> 'Rect':
+        newRect = copy(self)
+        newRect.height = height
+        return newRect
+
+    def with_size(self, size: 'Vec2') -> 'Rect':
+        newRect = copy(self)
+        newRect.width = size.x
+        newRect.height = size.y
+        return newRect
+
+    def expandUp(self, amount: int):
+        newRect = copy(self)
+        newRect.y -= amount
+        newRect.height += amount
+        return newRect
+    
+    def expandDown(self, amount: int):
+        newRect = copy(self)
+        newRect.height += amount
+        return newRect
+
+    def expandLeft(self, amount: int):
+        newRect = copy(self)
+        newRect.x -= amount
+        newRect.width += amount
+        return newRect
+
+    def expandRight(self, amount: int):
+        newRect = copy(self)
+        newRect.width += amount
+        return newRect
+
+    def expandCenter(self, amount: int):
+        newRect = copy(self)
+        newRect.x -= amount
+        newRect.y -= amount
+        newRect.width += amount * 2
+        newRect.height += amount * 2
+        return newRect
+
+    def moveRel(self, movementVec: Vec2):
+        newRect = copy(self)
+        newRect.x += movementVec.x
+        newRect.y += movementVec.y
+        return newRect
+
+    def moveRelX(self, amount: int):
+        newRect = copy(self)
+        newRect.x += amount
+        return newRect
+
+    def moveRelY(self, amount: int):
+        newRect = copy(self)
+        newRect.y += amount
+        return newRect
+
+    def moveAbs(self, newPivot: Vec2):
+        newRect = copy(self)
+        newRect.x = newPivot.x 
+        newRect.y = newPivot.y
+        return newRect
+
+    def __contains__(self, Vec2: Vec2) -> bool:
+        return (self.x <= Vec2.x < self.x + self.width and
+                self.y <= Vec2.y < self.y + self.height)
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Rect):
+            return False
+        return self.x == o.x and self.y == o.y and self.width == o.width and self.height == o.height
+
+    @classmethod
+    def from_bbox(self, bbox: tuple):
+        return Rect(bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1])
+
+    def cut_image(self, img: np.ndarray) -> np.ndarray:
+        sx, sy, w, h = self.x, self.y, self.width, self.height
+        ex, ey = sx + w, sy + h
+        return img[sy:ey, sx:ex]
+
+@dataclass
+class GRect(Rect):
     color: tuple = colors.pink # BGRA
     filled: bool = False
     visible: bool = True
@@ -110,51 +285,11 @@ class Rect(Serializable):
         # assert self.x >= 0, "x must be >= 0"
         # assert self.y >= 0, "y must be >= 0"
 
-    def center(self) -> Vec2:
-        return Vec2(self.x + self.width // 2, self.y + self.height // 2)
-    
-    def center_x(self) -> int:
-        return self.x + self.width // 2
-
-    def center_y(self) -> int:
-        return self.y + self.height // 2
-
-    def pivot(self) -> Vec2:
-        return Vec2(self.x, self.y)
-    
-    def size(self) -> Vec2:
-        return Vec2(self.width, self.height)
-
-    def to_bbox(self) -> tuple:
-        return (self.x, self.y, self.width + self.x, self.height + self.y)
-
-    def top_left(self) -> Vec2:
-        return Vec2(self.x, self.y)
-
-    def top_right(self) -> Vec2:
-        return Vec2(self.x + self.width, self.y)
-
-    def bottom_left(self) -> Vec2:
-        return Vec2(self.x, self.y + self.height)
-
-    def bottom_right(self) -> Vec2:
-        return Vec2(self.x + self.width, self.y + self.height)
-
-    def with_x(self, x: int) -> 'Rect':
-        return Rect(x, self.y, self.width, self.height)
-
-    def with_y(self, y: int) -> 'Rect':
-        return Rect(self.x, y, self.width, self.height)
-
-    @classmethod
-    def from_bbox(self, bbox: tuple):
-        return Rect(bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1])
-
     def __index__(self):
         return self.x, self.y, self.width, self.height, self.color, self.filled
 
     def __repr__(self):
-        return f'Rect(x={self.x}, y={self.y}, width={self.width}, height={self.height}, color={self.color}, filled={self.filled}, visible={self.visible})'
+        return f'GRect({self.x}, {self.y}, {self.width}, {self.height}, color={self.color}, filled={self.filled}, visible={self.visible})'
 
     def __iter__(self):
         yield from (self.x, self.y, self.width, self.height, self.color, self.filled, self.visible)
@@ -178,58 +313,7 @@ class Rect(Serializable):
         else:
             raise IndexError
 
-    def __contains__(self, Vec2: Vec2) -> bool:
-        return (self.x <= Vec2.x < self.x + self.width and
-                self.y <= Vec2.y < self.y + self.height)
-
-    def __eq__(self, o: object) -> bool:
-        return self.x == o.x and self.y == o.y and self.width == o.width and self.height == o.height
-
-    def __hash__(self):
-        return hash((self.x, self.y, self.width, self.height, self.color))
-
-    def expand(self, amount: Union[int, Vec2]):
-        if isinstance(amount, int):
-            return Rect(self.x - amount, self.y - amount, self.width + amount * 2, self.height + amount * 2, self.color, self.filled, self.visible)
-        elif isinstance(amount, Vec2):
-            return Rect(self.x - amount.x, self.y - amount.y, self.width + amount.x * 2, self.height + amount.y * 2, self.color, self.filled, self.visible)
-        else:
-            raise TypeError(f'Expected int or Vec2, got {type(amount)}')
-
-    def expandUp(self, amount: int):
-        return Rect(self.x, self.y - amount, self.width, self.height + amount, self.color, self.filled, self.visible)
-    
-    def expandDown(self, amount: int):
-        return Rect(self.x, self.y, self.width, self.height + amount, self.color, self.filled, self.visible)
-
-    def expandLeft(self, amount: int):
-        return Rect(self.x - amount, self.y, self.width + amount, self.height, self.color, self.filled, self.visible)
-
-    def expandRight(self, amount: int):
-        return Rect(self.x, self.y, self.width + amount, self.height, self.color, self.filled, self.visible)
-
-    def expandCenter(self, amount: int):
-        return Rect(self.x - amount, self.y - amount, self.width + amount * 2, self.height + amount * 2, self.color, self.filled)
-
-    def moveRel(self, movementVec: Vec2):
-        return Rect(self.x + movementVec.x, self.y + movementVec.y, self.width, self.height, self.color, self.filled, self.visible)
-
-    def moveRelX(self, amount: int):
-        return Rect(self.x + amount, self.y, self.width, self.height, self.color, self.filled, self.visible)
-
-    def moveRelY(self, amount: int):
-        return Rect(self.x, self.y + amount, self.width, self.height, self.color, self.filled, self.visible)
-
-    def moveAbs(self, newPivot: Vec2):
-        return Rect(newPivot.x, newPivot.y, self.width, self.height, self.color, self.filled, self.visible)
-
-    def copy(self):
-        return Rect(self.x, self.y, self.width, self.height, self.color, self.filled, self.visible)
-
     def colored(self, color: tuple):
-        return Rect(self.x, self.y, self.width, self.height, color, self.filled, self.visible)
+        return GRect(self.x, self.y, self.width, self.height, color, self.filled, self.visible)
 
-    def slice_frame(self, frame):
-        sx, sy, w, h = self.x, self.y, self.width, self.height
-        ex, ey = sx + w, sy + h
-        return frame[sy:ey, sx:ex]
+   
