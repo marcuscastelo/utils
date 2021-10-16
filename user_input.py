920224d8fb7ff7b2aee48dbd9e17e4e03c00c1ed
marcuscastelo <viniciuscastelo6@gmail.com>
2021-10-16 11:20:46 -0300

    Reset user input when bot clicks

--- user_input.py
+++ user_input.py
@@ -1,5 +1,4 @@
 import keyboard
-import pyautogui
 import time
 import pynput.mouse as pmouse
 
@@ -30,6 +29,13 @@ listener = pmouse.Listener(
         on_click=on_click,
         on_scroll=on_scroll)
 listener.start()
+
+def reset_user_input():
+    """
+    Ignores user input for a short period of time.
+    """
+    global userInputLastTime
+    userInputLastTime = 0
 
 def is_user_currently_using_keyboard_or_mouse():
     """
