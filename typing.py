02915492eeb89552148e77eb4bbb40a2a50b42b8
marcuscastelo <viniciuscastelo6@gmail.com>
2022-03-06 18:56:46 -0300

    setsig decorator

--- typing.py
+++ typing.py
@@ -1,6 +1,19 @@
 
+from typing import Any, Callable, TypeVar
 
-from decimal import Decimal
-from typing import Union
+T = TypeVar('T')
+def setsig(sig: T) -> Callable[[Any], T]:
+    '''Decorator to set the signature of a function to the given value.\n
+        Usage:\n
+            @setsig(print)\n
+            def my_print(*args, **kwargs):\n
+                do_stuff_before_print() # For example, start a timer here.\n
+                print(*args, **kwargs)\n
 
-Real = Union[float, int, Decimal]
+            if __name__ == '__main__':\n
+                # Works just like print('Hello', 'World'), with IDE, type checking, and other features \n
+                my_print('Hello', 'World') 
+    '''
+    def setsig_dec(func) -> T:
+        return func
+    return setsig_dec
